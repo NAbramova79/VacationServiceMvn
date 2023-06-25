eb5e591561b3b16f4a9e8a3c7492edd72493d5c4
NataA <nyabramova@gmail.com>
2023-06-25 20:34:00 +0200

    Resources

--- src/test/java/ru/netology/javaqa/vacationservicemvn/services/VacationCountServiceTest.java
+++ src/test/java/ru/netology/javaqa/vacationservicemvn/services/VacationCountServiceTest.java
@@ -3,15 +3,17 @@ package ru.netology.javaqa.vacationservicemvn.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.javaqa.vacationservicemvn.services.VacationCountService;
 
 public class VacationCountServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3",
-            "100000, 60000, 150000, 2"
-    })
+//    @CsvSource({
+//            "10000, 3000, 20000, 3",
+//            "100000, 60000, 150000, 2"
+//    })
+    @CsvFileSource(files="src/test/resources/vacation.csv")
     void shouldCalculateVacationMonths(int income, int expense, int threshold, int expected) {
         VacationCountService service = new VacationCountService();
 
